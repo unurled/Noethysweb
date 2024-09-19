8f9410b9d171fe7a3e114ab70463c5a6ac782606
Jean-Luc Peterschmitt <jean-luc.peterschmitt@logut.fr>
2024-09-20 00:29:44 +0200

    Modification de la méthode de mise à jour du code

--- noethysweb/noethysweb/settings.py
+++ noethysweb/noethysweb/settings.py
@@ -16,7 +16,7 @@ PORTAIL_ACTIF = True
 MODE_DEMO = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fdjsqfq543Z4645RFsdfSs564Z5-z(YZdqswFSDf'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
