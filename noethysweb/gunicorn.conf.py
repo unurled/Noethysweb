3345ee19b819d27ba2ba74cd899aa83e69a0dfc5
Jean-Luc Peterschmitt <jean-luc.peterschmitt@logut.fr>
2025-03-04 02:30:51 +0100

    Add gunicorn logging config

--- noethysweb/gunicorn.conf.py
+++ noethysweb/gunicorn.conf.py
@@ -6,4 +6,4 @@ limit_request_line = 8190
 
 logger_class = "glogging.Logger"
 accesslog = "/var/www/vhosts/flambeaux.org/logs/sacadoc.flambeaux.org/gunicorn.log"
-access_log_format = '{"timestamp":"%({request_start}c)s","server":"gunicorn","ip":"%({x-real-ip}i)s","vhost":"sacadoc.flambeaux.org","c_host":"%({host}i)s","method":"%(m)s","uri":"%(U)s","qs":"%(q)s","status":"%(s)s","timetakenms":"%(M)s","size":"%(B)s","ua":"%(a)s","ref":"%(f)s","outct":"%({content-type}o)s","user":"%({x-user}o)s","uid":"%({x-uid}i)s","country":"%({x-country-code}i)s"}'
+access_log_format = '{"datetime":"%({request_start}c)s","server":"gunicorn","ip":"%({x-real-ip}i)s","vhost":"sacadoc.flambeaux.org","c_host":"%({host}i)s","method":"%(m)s","uri":"%(U)s","qs":"%(q)s","status":"%(s)s","timetakenms":"%(M)s","size":"%(B)s","ua":"%(a)s","ref":"%(f)s","outct":"%({content-type}o)s","user":"%({x-user}o)s","uid":"%({x-uid}i)s","country":"%({x-country-code}i)s"}'
