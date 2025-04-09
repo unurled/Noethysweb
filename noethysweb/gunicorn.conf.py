d1852d3de3911c087587735fab2fadc2de461dd5
Jean-Luc Peterschmitt <jean-luc.peterschmitt@logut.fr>
2025-04-10 00:54:26 +0200

    Allonger timeout gunicorn

--- noethysweb/gunicorn.conf.py
+++ noethysweb/gunicorn.conf.py
@@ -7,3 +7,5 @@ limit_request_line = 8190
 logger_class = "glogging.Logger"
 accesslog = "/var/www/vhosts/flambeaux.org/logs/sacadoc.flambeaux.org/gunicorn.log"
 access_log_format = '{"datetime":"%({request_start}c)s","server":"gunicorn","ip":"%({x-real-ip}i)s","vhost":"sacadoc.flambeaux.org","c_host":"%({host}i)s","method":"%(m)s","uri":"%(U)s","qs":"%(q)s","status":"%(s)s","timetakenms":"%(M)s","size":"%(B)s","ua":"%(a)s","ref":"%(f)s","outct":"%({content-type}o)s","user":"%({x-user}o)s","uid":"%({x-uid}i)s","country":"%({x-country-code}i)s"}'
+
+timeout = 3600 # Needed for batch mail sending which is done sync with a request for now
