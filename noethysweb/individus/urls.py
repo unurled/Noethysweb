8f9b7572e9343d33c5d103559d36d457bbf8d1a2
Arthur67190 <152179550+Arthur67190@users.noreply.github.com>
2025-04-22 16:32:46 +0200

    s

--- noethysweb/individus/urls.py
+++ noethysweb/individus/urls.py
@@ -210,4 +210,5 @@ urlpatterns = [
     path('individus/imprimer_liste_inscrits/generer_csv', secure_ajax(imprimer_liste_inscrits.Generer_csv), name='ajax_imprimer_liste_inscrits_generer_csv'),
     path('individus/famille_attestations/generer_pdf', secure_ajax(famille_attestations.Generer_pdf), name='ajax_famille_attestations_generer_pdf'),
     path('individus/questionnaires/individus/relance/', secure_ajax(liste_questionnaires_individus.traiter_relance), name='ajax_get_relance'),
+
 ]
