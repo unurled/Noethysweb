b39d6d7932d5f293aba6d437ddc297f866cb2ea5
unurled <unurled@unurled.me>
2025-11-08 23:31:19 +0100

    fix: avertissement de renseignements manquants dans la fiche famille coté directeur

--- noethysweb/fiche_famille/views/famille.py
+++ noethysweb/fiche_famille/views/famille.py
@@ -19,7 +19,7 @@ from individus.utils import utils_pieces_manquantes
 from fiche_individu.forms.individu import Formulaire
 from fiche_famille.utils.utils_famille import LISTE_ONGLETS
 from cotisations.utils import utils_cotisations_manquantes
-
+from portail.utils import utils_renseignements_manquants
 
 def Definir_titulaire(request):
     # Récupération des données du formulaire
@@ -226,7 +226,9 @@ class Resume(Onglet, DetailView):
         # Alertes
         context['pieces_fournir'] = utils_pieces_manquantes.Get_pieces_manquantes(famille=context['famille'], only_invalides=True, utilisateur=self.request.user)
         context['cotisations_manquantes'] = utils_cotisations_manquantes.Get_cotisations_manquantes(famille=context['famille'])
-        context['nbre_alertes'] = len(context['pieces_fournir']) + len(context['cotisations_manquantes'])
+        context['renseignements_manquants'] = utils_renseignements_manquants.Get_renseignements_manquants(famille=context['famille'])
+        context['nbre_alertes'] = len(context['pieces_fournir']) + len(context['cotisations_manquantes']) + len(context['renseignements_manquants'])
+        context['hover_message'] = context['renseignements_manquants']['message_manquant']
 
         # Notes
         conditions = (Q(utilisateur=self.request.user) | Q(utilisateur__isnull=True)) & (Q(structure__in=self.request.user.structures.all()) | Q(structure__isnull=True))
